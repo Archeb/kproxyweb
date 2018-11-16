87ca59b623903163ed9c04415efb55d6e6e85344
Archeb <archebasic@hotmail.com>
2018-11-16 22:27:06 +0800

    添加工具条

--- clean.php
+++ clean.php
@@ -1,4 +1,9 @@
 <?php
+if(isset($_GET['hidemenu'])){
+    setcookie('kp_hide_menu','1');
+    header("location:".$_SERVER["HTTP_REFERER"]);
+    die();
+}
 foreach ($_COOKIE as $key => $value) {
         setcookie($key, null);
     }
